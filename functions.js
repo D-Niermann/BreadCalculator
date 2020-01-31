0d3e9ac9d357cd02fbdd012e6175586c759ca9e5
D-Niermann <dario.niermann@gmail.com>
2020-01-31 18:24:55 +0100

    added event listeners

--- functions.js
+++ functions.js
@@ -1,13 +1,38 @@
+var closebtns = document.getElementsByClassName("close");
+var i;
 
+/* Loop through the elements, and hide the parent, when clicked on */
+for (i = 0; i < closebtns.length; i++) {
+  console.log(i)
+  closebtns[i].addEventListener("click", function() {
+  this.parentElement.style.display = 'none';
+});
+}
 
+////////////////////////////////////////////
+// ZUTAT SPALTE
+////////////////////////////////////////////
 document.getElementById("add").addEventListener("click", function() {
     var list = document.getElementById('list');
     var entry = document.createElement('li');
-   
+    var close = document.createElement("span")
+    close.className = "close"
+    close.innerHTML = "x"
+    close.addEventListener("click", function() {
+      this.parentElement.style.display = 'none';
+    });
+
     var input = document.createElement("input");
     input.type = "text";
     input.className = "input-text-gramm"; // set the CSS class
     input.value = "0"
+    input.addEventListener ('keydown', function (event) {
+      if (event.which == 13) { //enter key
+         event.preventDefault();
+         let field = event.target;
+        console.log("Updated Grams")
+      }
+    });
     entry.appendChild(input); // put it into the DOM
    
     var input = document.createElement("input");
@@ -36,15 +61,16 @@ document.getElementById("add").addEventListener("click", function() {
     entry.appendChild(sel)
 
 
+    entry.appendChild(close)
     list.appendChild(entry);
 });
-
+////////////////////////////////////////////////////////////////////////////////
 
 // key down event with enter key
-// document.getElementById('form').addEventListener ('keydown', function (event) {
-//   if (event.which == 13) { //enter key
-//      event.preventDefault();
-//      let field = event.target;
+document.getElementById('form').addEventListener ('keydown', function (event) {
+  if (event.which == 13) { //enter key
+     event.preventDefault();
+     let field = event.target;
     
-//   }
-// });
+  }
+});
